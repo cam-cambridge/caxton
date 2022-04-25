4ed64ebd57b8800dbce0e64682cee3e6f8a080f0
Douglas Brion <me@douglasbrion.com>
2022-04-25 11:56:14 +0100

    Training files

---
+++ src/train.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import ModelCheckpoint
+from data.data_module import ParametersDataModule
+from model.network_module import ParametersClassifier
+
+from train_config import *
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-s", "--seed", default=1234, type=int, help="Set seed for training"
+)
+parser.add_argument(
+    "-e",
+    "--epochs",
+    default=MAX_EPOCHS,
+    type=int,
+    help="Number of epochs to train the model for",
+)
+
+args = parser.parse_args()
+seed = args.seed
+
+set_seed(seed)
+logs_dir = "logs/logs-{}/{}/".format(DATE, seed)
+logs_dir_default = os.path.join(logs_dir, "default")
+
+make_dirs(logs_dir)
+make_dirs(logs_dir_default)
+
+tb_logger = pl_loggers.TensorBoardLogger(logs_dir)
+checkpoint_callback = ModelCheckpoint(
+    monitor="val_loss",
+    dirpath="checkpoints/{}/{}/".format(DATE, seed),
+    filename="MHResAttNet-{}-{}-".format(DATASET_NAME, DATE)
+    + "{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}",
+    save_top_k=3,
+    mode="min",
+)
+
+model = ParametersClassifier(
+    num_classes=3,
+    lr=INITIAL_LR,
+    gpus=NUM_GPUS,
+    transfer=False,
+)
+
+data = ParametersDataModule(
+    batch_size=BATCH_SIZE,
+    data_dir=DATA_DIR,
+    csv_file=DATA_CSV,
+    dataset_name=DATASET_NAME,
+    mean=DATASET_MEAN,
+    std=DATASET_STD,
+    load_saved=True,
+)
+
+trainer = pl.Trainer(
+    num_nodes=NUM_NODES,
+    gpus=NUM_GPUS,
+    distributed_backend=ACCELERATOR,
+    max_epochs=args.epochs,
+    logger=tb_logger,
+    weights_summary=None,
+    precision=16,
+    callbacks=[checkpoint_callback],
+)
+
+trainer.fit(model, data)
---
+++ src/train_config.py
@@ -0,0 +1,60 @@
+import os
+from datetime import datetime
+import numpy as np
+import torch
+from pytorch_lightning import seed_everything
+
+DATE = datetime.now().strftime("%d%m%Y")
+
+dataset_switch = 1
+
+DATA_DIR = os.environ.get("DATA_DIR")
+
+if dataset_switch == 0:
+    DATASET_NAME = "dataset_single_layer"
+    DATA_CSV = os.path.join(
+        DATA_DIR,
+        "caxton_dataset/caxton_dataset_filtered_single.csv",
+    )
+    DATASET_MEAN = [0.16853632, 0.17632364, 0.10495131]
+    DATASET_STD = [0.05298341, 0.05527821, 0.04611006]
+elif dataset_switch == 1:
+    DATASET_NAME = "dataset_full"
+    DATA_CSV = os.path.join(
+        DATA_DIR,
+        "caxton_dataset/caxton_dataset_filtered.csv",
+    )
+    DATASET_MEAN = [0.2915257, 0.27048784, 0.14393276]
+    DATASET_STD = [0.066747, 0.06885352, 0.07679665]
+elif dataset_switch == 2:
+    DATASET_NAME = "dataset_equal"
+    DATA_CSV = os.path.join(
+        DATA_DIR,
+        "caxton_dataset/caxton_dataset_filtered_equal.csv",
+    )
+    DATASET_MEAN = [0.2925814, 0.2713622, 0.14409496]
+    DATASET_STD = [0.0680447, 0.06964592, 0.0779964]
+
+INITIAL_LR = 0.001
+
+BATCH_SIZE = 32
+MAX_EPOCHS = 50
+
+NUM_NODES = 1
+NUM_GPUS = 2
+ACCELERATOR = "ddp"
+
+
+def set_seed(seed):
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
+    seed_everything(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+
+def make_dirs(path):
+    try:
+        os.makedirs(path)
+    except:
+        pass
